944eede6034559fccce43a331817077b24dc4cf9
jkalasas <alasasjohnkyle@gmail.com>
2025-09-02 12:50:41 +0800

    fix: tuari 2 implementation of system trays

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,34 +1,20 @@
-use tauri::{
-    AppHandle, CustomMenuItem, GlobalWindowEvent, SystemTray, SystemTrayEvent, SystemTrayMenu,
-    SystemTrayMenuItem, WindowEvent,
-};
-use tauri::Manager;
+use tauri::{AppHandle, Manager};
+use tauri::WindowEvent;
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+use tauri::tray::TrayIconBuilder;
 
-fn handle_window_event(event: GlobalWindowEvent) {
-    match event.event() {
-        WindowEvent::CloseRequested { api, .. } => {
-            api.prevent_close();
-            event.window().hide().unwrap();
+fn handle_menu_event(app: &AppHandle, id: &str) {
+    match id {
+        "quit" => app.exit(0),
+        "hide" => {
+            if let Some(win) = app.get_webview_window("main") {
+                let _ = win.hide();
+            }
         }
-        _ => {}
-    }
-}
-
-fn handle_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
-    match event {
-        SystemTrayEvent::MenuItemClick { id, .. } => {
-            let window = app.get_window("main").unwrap();
-            match id.as_str() {
-                "quit" => {
-                    app.exit(0);
-                }
-                "hide" => {
-                    window.hide().unwrap();
-                }
-                "show" => {
-                    window.show().unwrap();
-                }
-                _ => {}
+        "show" => {
+            if let Some(win) = app.get_webview_window("main") {
+                let _ = win.show();
+                let _ = win.set_focus();
             }
         }
         _ => {}
@@ -37,24 +23,32 @@ fn handle_system_tray_event(app: &AppHandle, event: SystemTrayEvent) {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-    let show = CustomMenuItem::new("show".to_string(), "Show");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(show)
-        .add_item(hide)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit);
-
-    let system_tray = SystemTray::new().with_menu(tray_menu);
-
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
-        .system_tray(system_tray)
-        .on_system_tray_event(handle_system_tray_event)
-        .on_window_event(handle_window_event)
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                api.prevent_close();
+                let _ = window.hide();
+            }
+        })
         .setup(|app| {
+            // Build tray menu using tauri v2 menu API
+            let menu = MenuBuilder::new(app)
+                .item(&MenuItemBuilder::with_id("show", "Show").build(app)?)
+                .item(&MenuItemBuilder::with_id("hide", "Hide").build(app)?)
+                .separator()
+                .item(&MenuItemBuilder::with_id("quit", "Quit").build(app)?)
+                .build()?;
+
+            // Create tray icon and wire up menu events
+        TrayIconBuilder::new()
+                .menu(&menu)
+                .on_menu_event(|app, event| {
+            handle_menu_event(app, event.id.as_ref());
+                })
+                .build(app)?;
+
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
